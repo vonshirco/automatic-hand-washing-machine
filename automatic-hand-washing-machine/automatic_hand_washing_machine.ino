5125726e434e99d4e7fb408a7b2419233ec400f7
Von <vonshirco.vs@gmail.com>
2022-03-21 23:39:53 +0300

    Update automatic_hand_washing_machine.ino

--- automatic-hand-washing-machine/automatic_hand_washing_machine.ino
+++ automatic-hand-washing-machine/automatic_hand_washing_machine.ino
@@ -1,5 +1,14 @@
 //Project Descriptions
 
+//Declaring constants and variables
+#define trig 5
+#define echo 4
+int waterRelay = 6;
+int soapRelay = 7;
+long duration;
+int distance;
+
+
 void setup() {
   // put your setup code here, to run once:
 
